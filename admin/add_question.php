8089eabfd6c10e018ec91dfcb2f241cb0be5b258
C.K.Chan <ck.chan1225@yahoo.com.hk>
2015-09-26 19:55:41 +0800

    add question.php

---
+++ admin/add_question.php
@@ -0,0 +1,27 @@
+<?php include 'includes/header.php';?>
+<div class="container">
+<h2>Add questions</h2>
+<form method="post" action="add_question.php">
+  <div class="form-group">
+    <label>Year</label>
+    <input name="year" type="text" class="form-control"  placeholder="Enter Year">
+  </div>
+   <div class="form-group">
+    <label>Question Number</label>
+    <input name="number" type="text" class="form-control"  placeholder="Enter Question Number">
+  </div>
+   <div class="form-group">
+    <label>Year</label>
+    <input name="year" type="text" class="form-control"  placeholder="Enter Year">
+  </div>
+   <div class="form-group">
+    <label>Year</label>
+    <input name="year" type="text" class="form-control"  placeholder="Enter Year">
+  </div>
+   <div class="form-group">
+    <label>Year</label>
+    <input name="year" type="text" class="form-control"  placeholder="Enter Year">
+  </div>
+  <button name="submit" type="submit" class="btn btn-default">Submit</button>
+</form>
+</div>
